c9251f9bee87857a8fe06f4c62774f6059db4a5b
soyudea <omara.torres@udea.edu.co>
2025-06-15 14:18:51 -0500

    proyecto de prueba

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mi primera app", page_icon="🚀")
+
+st.title("¡Hola desde Streamlit! 👋")
+st.write("Esta es una aplicación sencilla desplegada en Streamlit Cloud.")
+st.success("¡Funciona perfectamente!")
