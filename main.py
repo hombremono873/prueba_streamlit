cf61f8d6cbdbfd1d508c974a44e99d893524e03a
soyudea <omara.torres@udea.edu.co>
2025-06-15 14:33:54 -0500

    Actualizado

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ st.set_page_config(page_title="Mi primera app", page_icon="🚀")
 st.title("¡Hola desde Streamlit! 👋")
 st.write("Esta es una aplicación sencilla desplegada en Streamlit Cloud.")
 st.success("¡Funciona perfectamente!")
+
+st.success(f"Exito hola mundo")
